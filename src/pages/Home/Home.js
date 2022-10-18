ff2de70dcb3fbe365bb035d043890575c0dcc57b
jfbarkokebas <jfbarkokebas@gmail.com>
2022-10-18 18:49:22 -0300

    create carousel in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,88 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState, useRef } from 'react'
 import api from '../../services/api'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './Home.css'
 
+function Home() {
+
+    const [movies, setMovies] = useState([])
+    const carousel = useRef(null)
+
+    useEffect(() => {
+        async function loadMovies(){
+
+            //setIsLoading(true)
+
+            const response = await api.get('movie/now_playing',{
+                params:{
+                    api_key: '90d6ea616ca99198ac773904c66275c4',
+                    language:'pt-BR',
+                    page:1,
+
+                }
+            })
+
+            console.log(response);
+            setMovies(response.data.results)
+            //setIsLoading(false)
+            
+        }
+
+        loadMovies()
+
+    }, [])
+
+    if (!movies || movies.length === 0) return null
+
+    function handleRigth(e) {
+        e.preventDefault()
+        console.log(carousel.current.offsetWidth);
+        carousel.current.scrollLeft -= carousel.current.offsetWidth
+
+    }
+
+    function handleLeft(e) {
+        e.preventDefault()
+        console.log(carousel.current);
+        carousel.current.scrollLeft += carousel.current.offsetWidth
+
+    }
+
+    return (
+        <div className="container">
+
+            
+
+            <button id="left-btn" onClick={handleRigth}><img src="/static/images/216151_right_chevron_icon.png" alt="botão-direita" />
+                </button>
+
+           
+            
+            <div className="carousel" ref={carousel}>
+                {movies && movies.map(item =>
+                    <div className="item" key={item.id}>
+                       <strong>{item.title}</strong>
+                        <img src={`https://image.tmdb.org/t/p/original/${item.backdrop_path}`} alt="imagem do poster do filme" />
+                        <Link to={`/movie/${item.id}`}>Acessar</Link>
+                    </div>
+                )}
+            </div>
+            
+                <button id="rigth-btn" onClick={handleLeft}><img src="/static/images/216151_right_chevron_icon.png" alt="botão-esquerda" />
+                </button>
+            
+
+        </div>
+    );
+}
+
+/*
 const Home = () => {
     const [filmes, setFilmes] = useState([])
     const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
-        async function loadFilmes(){
+        async function loadMovies(){
 
             setIsLoading(true)
 
@@ -22,14 +96,14 @@ const Home = () => {
             })
 
             console.log(response);
-            setFilmes(response.data.results)
+            setFilmes(response.movie.results)
             setIsLoading(false)
             
         }
 
-        loadFilmes()
+        loadMovies()
 
-    }, [])
+    }, []) 
 
     if(isLoading){
         return(
@@ -42,6 +116,7 @@ const Home = () => {
 
     return (
         <div className='container'>
+           
             <div className="lista-filmes">
                 {filmes && filmes.map(f=>(
                     <article key={f.id}>
@@ -50,9 +125,9 @@ const Home = () => {
                         <Link to={`/movie/${f.id}`}>Acessar</Link>
                     </article>
                 ))}
-            </div>
+                </div>
         </div>
     )
-}
+} */
 
 export default Home
